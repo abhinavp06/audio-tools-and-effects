a24ebfab0c8cdacc01601ab0c16ceaff7d29e1b3
abhinavp06 <aparasharchd06@gmail.com>
2026-03-21 01:50:07 +0530

    sine wave generator

--- src/effects/generators/wave/sine/sine_wave.cpp
+++ src/effects/generators/wave/sine/sine_wave.cpp
@@ -1,5 +1,6 @@
 ﻿#include "sine_wave.h"
-#include <random>
+#include <cmath>
+#include <numbers>
 
 /*
 * y(t) = A * sin(2π * f * t + φ)
@@ -14,7 +15,31 @@
 */
 
 AudioBuffer SineWave::generate_naive(const int duration_s, const int sample_rate, const int channel_count, const double frequency, const double amplitude) {
+	if (duration_s <= 0) {
+		throw std::invalid_argument("Duration must be a positive integer!");
+	}
+	if (channel_count <= 0) {
+		throw std::invalid_argument("Channel count must be a positive integer!");
+	}
+	if (frequency > static_cast<double>(sample_rate) / 2) {
+		throw std::invalid_argument("Frequency must be greater than or equal to half of the sample rate!");
+	}
+	if (amplitude < 0.0 || amplitude > 1.0) {
+		throw std::invalid_argument("Amplitude must be between 0.0 and 1.0!");
+	}
 
+	AudioBuffer sine_wave_buffer;
+	sine_wave_buffer.channels = channel_count;
+	sine_wave_buffer.sampleRate = sample_rate;
+
+	const size_t sample_count = static_cast<size_t>(duration_s) * sample_rate * channel_count;
+
+	sine_wave_buffer.samples.resize(sample_count);
+	for (size_t i = 0; i < sample_count; i++) {
+		sine_wave_buffer.samples[i] = amplitude * std::sin(2 * std::numbers::pi * frequency * (static_cast<double>(i/channel_count) / sample_rate)); // i/channel_count is required to accumulate for channel interleaving
+	}
+
+	return sine_wave_buffer;
 }
 
 /*
@@ -34,5 +59,37 @@ AudioBuffer SineWave::generate_naive(const int duration_s, const int sample_rate
 *
 */
 AudioBuffer SineWave::generate(const int duration_s, const int sample_rate, const int channel_count, const double frequency, const double amplitude) {
+	if (duration_s <= 0) {
+		throw std::invalid_argument("Duration must be a positive integer!");
+	}
+	if (channel_count <= 0) {
+		throw std::invalid_argument("Channel count must be a positive integer!");
+	}
+	if (frequency > static_cast<double>(sample_rate) / 2) {
+		throw std::invalid_argument("Frequency must be greater than or equal to half of the sample rate!");
+	}
+	if (amplitude < 0.0 || amplitude > 1.0) {
+		throw std::invalid_argument("Amplitude must be between 0.0 and 1.0!");
+	}
 
+	AudioBuffer sine_wave_buffer;
+	sine_wave_buffer.channels = channel_count;
+	sine_wave_buffer.sampleRate = sample_rate;
+
+	const size_t sample_count = static_cast<size_t>(duration_s) * sample_rate * channel_count;
+
+	sine_wave_buffer.samples.resize(sample_count);
+	double phase_increment = 2 * std::numbers::pi * frequency / sample_rate, phase = 0.0;
+	for (size_t i = 0; i < sample_count; i++) {
+		sine_wave_buffer.samples[i] = amplitude * std::sin(phase);
+		if (i % channel_count == channel_count - 1) { // channel_interleaving
+			phase += phase_increment;
+
+			if (phase >= 2 * std::numbers::pi) {
+				phase -= 2 * std::numbers::pi;
+			}
+		}
+	}
+
+	return sine_wave_buffer;
 }
