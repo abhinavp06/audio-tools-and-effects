29616b464e531c9c8f1bd24af14ccbd12de3ab0c
abhinavp06 <aparasharchd06@gmail.com>
2026-04-02 08:40:48 +0530

    added feedback delay header

---
+++ include/atae/delay/feedback_delay.h
@@ -0,0 +1,16 @@
+#include "atae/delay/delay_line.h"
+
+class FeedbackDelay {
+public:
+	void prepare(int sample_rate, float max_delay_ms);
+	void setDelay(float delay_ms);
+	void setFeedback(float feedback);
+	void setWetMix(float wm);
+	float processSample(float input);
+	void reset();
+
+private:
+	DelayLine delay_line;
+	float feedback_coefficient = 0.0f;
+	float wet_mix = 0.0f;
+};
